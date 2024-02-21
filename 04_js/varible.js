05a4b84d9c9d954dc3ffa1b924af12122fa6e8ec
bug0630 <bugbdh@gmail.com>
2024-02-21 16:59:40 +0900

    js

---
+++ 04_js/varible.js
@@ -0,0 +1,40 @@
+let a = 5;
+let b = 3;
+alert(a + b);
+// 변수선언
+var varName;
+varName = "layla";
+
+console.log(varName);
+// 변수 재할당
+var name = "yuki";
+console.log(varName);
+// 변수 선언과 값 할당을 동시에
+
+var varName2 = "layla2";
+
+// var의 이상한 점
+// 변수를 동일한 이름으로 재선언 할 수 있음
+var varName = "lily";
+
+console.log(varName); //lily를 출력
+// 2. 변수를 선언하기 전 값을 할당할 수 있음
+// 유지보수할 때 최악
+aa = 123;
+console.log(aa);
+
+//let 키워드
+// 1. 변수 선언->값 할당
+// 2.변수 선언과 값 할당을 동시에
+
+// 변수 선언
+let letName;
+// 값 할당
+letName = "홍길동";
+//변수 선언과 값 할당을 동시에
+let letName2 = "심청";
+alert(letName2);
+// 중복 선언 불가
+
+letName2 = "심봉사"; //재할당 가능
+console.log(letName2);
